56213ae939c8cfe30a217399d5e61e0653979f04
Jon Andre <jasolber@stud.ntnu.no>
2024-04-29 12:32:46 +0200

    Added comments

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,11 @@ import (
 	"log"
 )
 
+// main is the entry point for the program.
 func main() {
+	// Connect to the database.
 	database := database_2024.Connect()
+	// Close the database connection when the main function returns.
 	defer func(db *sql.DB) {
 		err := db.Close()
 		if err != nil {
