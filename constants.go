14aec8206a51901475036fb75c114264c45c5d6e
Jon Andre <jasolber@stud.ntnu.no>
2024-04-29 12:32:20 +0200

    Created constants.go and added constants for the Data Source Name

---
+++ constants.go
@@ -0,0 +1,13 @@
+package database_2024
+
+// DATABASE_USER is the username for the database.
+const DATABASE_USER = "dev"
+
+// DATABASE_PASSWORD is the password for the database.
+const DATABASE_PASSWORD = "Devdevdev1!"
+
+// DATABASE_NAME is the name of the database.
+const DATABASE_NAME = "ElectroMart"
+
+// DATABASE_IP is the IP address and port of the database.
+const DATABASE_IP = "10.212.169.49:3306"
