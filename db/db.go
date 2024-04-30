49e9719a9bc9fe2cf430831b2c443571e51f7e92
Simon <106014761+simonhoumb@users.noreply.github.com>
2024-04-30 10:54:33 +0200

    refactor: re-added db functions

--- db/db.go
+++ db/db.go
@@ -1,11 +1,15 @@
 package db
 
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -58,5 +62,47 @@ func GetCategoriesHandler(db *sql.DB) http.HandlerFunc {
 
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(jsonData)
+	}
+
+}
+
+// getDataSourceName returns a Data Source Name string for connecting to a MySQL database.
+func getDataSourceName() string {
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbNet := os.Getenv("DB_NET")
+	dbIP := os.Getenv("DB_IP")
+	dbName := os.Getenv("DB_NAME")
+
+	cfg := mysql.Config{
+		User:                 dbUser,
+		Passwd:               dbPassword,
+		Net:                  dbNet,
+		Addr:                 dbIP,
+		DBName:               dbName,
+		AllowNativePasswords: true,
+	}
+
+	return cfg.FormatDSN()
+}
+
+// openDatabaseConnection opens a connection to the database and returns a pointer to the database.
+func openDatabaseConnection() *sql.DB {
+	// Open a database connection.
+	db, err := sql.Open("mysql", getDataSourceName())
+	if err != nil {
+		log.Fatal("Error when opening the database: ", err)
+	}
+	return db
+}
+
+// pingDatabase pings the database to verify the connection.
+func pingDatabase(db *sql.DB) {
+	// Ping the database to verify the connection.
+	ctx, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	err := db.PingContext(ctx)
+	if err != nil {
+		log.Fatal("Error when pinging database: ", err)
 	}
 }
