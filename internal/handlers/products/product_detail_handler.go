7b6c08e767154cedcf01f57e6f843872e55189dc
Erik Bjørnsen <bjornsen.erik@gmail.com>
2024-05-01 12:57:26 +0200

    wip: added most of functionality of getbyid handler, commit before merge

--- internal/handlers/products/product_detail_handler.go
+++ internal/handlers/products/product_detail_handler.go
@@ -1,6 +1,9 @@
 package products
 
 import (
+	"Database_Project/internal/db"
+	"Database_Project/internal/utils"
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -37,7 +40,29 @@ func HandleProductDetail(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleGetDetailRequest(w http.ResponseWriter, r *http.Request) {
+	id, err := utils.GetIDFromRequest(r)
+	if utils.HandleError(w, r, http.StatusBadRequest, err, "Error getting ID from request") {
+		return
+	}
 
+	// Get the product with the given ID
+	products, err := db.GetAllProducts(db.Client)
+	if utils.HandleError(w, r, http.StatusInternalServerError, err, "Error getting products from database") {
+		return
+	}
+
+	// Take first product from the list TODO: Change this to get the product with the given ID
+	product := products[0]
+	product.ID = product.ID + id
+
+	// Return the product
+	if marshalledProduct, err := json.Marshal(product); utils.HandleError(w, r, http.StatusInternalServerError, err, "Error during encoding response") {
+		return
+	} else {
+		if _, err := w.Write(marshalledProduct); utils.HandleError(w, r, http.StatusInternalServerError, err, "Error writing response") {
+			return
+		}
+	}
 }
 
 func handleUpdateDetailRequest(w http.ResponseWriter, r *http.Request) {
